679c1863de7abf3cd5d40fcdbabaa61c1c02629c
ozline <ozlinex@outlook.com>
2025-07-01 15:38:46 +0800

    chore: format comment

--- benchmark/strings/replacer_vs_replaceAll_test.go
+++ benchmark/strings/replacer_vs_replaceAll_test.go
@@ -37,25 +37,24 @@ func BenchmarkReplacer(b *testing.B) {
 }
 
 /*
+	goos: darwin
+	goarch: arm64
+	pkg: github.com/ozline/go-codes/language
+	cpu: Apple M1 Pro
+	=== RUN   BenchmarkReplaceAll
+	BenchmarkReplaceAll
+	BenchmarkReplaceAll-10           9238455               124.1 ns/op            32 B/op          1 allocs/op
+	PASS
+	ok      github.com/ozline/go-codes/language     1.463s
 
-goos: darwin
-goarch: arm64
-pkg: github.com/ozline/go-codes/language
-cpu: Apple M1 Pro
-=== RUN   BenchmarkReplaceAll
-BenchmarkReplaceAll
-BenchmarkReplaceAll-10           9238455               124.1 ns/op            32 B/op          1 allocs/op
-PASS
-ok      github.com/ozline/go-codes/language     1.463s
 
-
-goos: darwin
-goarch: arm64
-pkg: github.com/ozline/go-codes/language
-cpu: Apple M1 Pro
-=== RUN   BenchmarkReplacer
-BenchmarkReplacer
-BenchmarkReplacer-10            14662659                78.77 ns/op           40 B/op          2 allocs/op
-PASS
-ok      github.com/ozline/go-codes/language     1.427s
+	goos: darwin
+	goarch: arm64
+	pkg: github.com/ozline/go-codes/language
+	cpu: Apple M1 Pro
+	=== RUN   BenchmarkReplacer
+	BenchmarkReplacer
+	BenchmarkReplacer-10            14662659                78.77 ns/op           40 B/op          2 allocs/op
+	PASS
+	ok      github.com/ozline/go-codes/language     1.427s
 */
