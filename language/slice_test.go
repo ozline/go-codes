e4ce1859a2bdd4f94a4b5d05521f6823560ed49e
ozline <ozlinex@outlook.com>
2025-07-04 10:58:27 +0800

    add: slice_test comments

--- language/slice_test.go
+++ language/slice_test.go
@@ -17,9 +17,15 @@ func TestRunSlicePrint(t *testing.T) {
 	fmt.Println(string(b))
 	fmt.Println(string(c))
 
+	// 如果 append 了 3 个C
 	// AAAACCCBB
 	// AAAACCC
 	// CCBB
+
+	// 如果 append 了一堆 C，比如 16 个 C
+	// AAAA/BBBB
+	// AAAACCCCCCCCCCCCCC
+	// BBBB
 
 	// 切片是对底层数组的引用，修改切片会影响原始切片
 	// 参考文章：https://blog.csdn.net/weixin_45304503/article/details/138639725
