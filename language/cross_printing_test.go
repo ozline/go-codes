44fdbf32fd75fa38558a52687807c8bca5a6034e
ozline <ozlinex@outlook.com>
2025-08-06 10:57:43 +0800

    add: extend cross printing test to N secnario

--- language/cross_printing_test.go
+++ language/cross_printing_test.go
@@ -5,6 +5,7 @@ package code
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 )
 
@@ -32,4 +33,51 @@ func TestCrossPrinting(t *testing.T) {
 	numChan <- struct{}{}
 
 	fmt.Println()
+}
+
+// 和上一个函数的区别在于，这个函数逻辑是N 个（N 由用户定义）goroutine 交替打印 1-100 的数字
+func TestCrossPrintingN(t *testing.T) {
+	num := 100
+	n := 50 // 定义 N 个 goroutine，实际 goroutine 取值是 min(num, n)
+	wg := sync.WaitGroup{}
+
+	// 创建 N 个 channel
+	channels := make([]chan struct{}, n)
+	for i := 0; i < n; i++ {
+		channels[i] = make(chan struct{})
+	}
+
+	current := 1
+	done := make(chan struct{}) // 用于通知 goroutine 停止
+
+	for i := range n {
+		wg.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+			for {
+				select {
+				case <-channels[idx]: // 等待当前 channel 的信号
+					if current > num {
+						// 如果超过最大值，通知所有 goroutine 停止
+						close(done)
+						return
+					}
+					t.Logf("goroutine %d: %d\n", idx+1, current)
+					current++
+					// 发送信号给下一个 goroutine
+					channels[(idx+1)%n] <- struct{}{}
+				case <-done: // 如果收到停止信号，退出
+					return
+				}
+			}
+		}(i)
+	}
+
+	// 启动第一个 goroutine
+	channels[0] <- struct{}{}
+
+	// 等待所有 goroutine 完成
+	wg.Wait()
+
+	fmt.Println("Finished printing numbers")
 }
